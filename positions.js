6f1744a18c18ad05b8f4d3ed35ac662d53bd86ba
cuisquare <cuisquare@gmail.com>
2024-03-09 12:25:35 +0000

    draggable positions!

--- positions.js
+++ positions.js
@@ -96,20 +96,104 @@ class Team {
 }
 
 class Lineup {
-    constructor(shirtnums = [15,16,17,18,19,20], symbols = ["S","O1","M1","Opp","O2","M2"], maxshirtnum = 999) {
+    constructor(shirtnums = [15,16,17,18,19,20], symbols = ["S","O1","M1","Opp","O2","M2"], lucontext) {
         this.shirtnums = shirtnums;
         this.symbols = symbols;
-        this.positions = this.getPositions(shirtnums, symbols);
+        this.context = lucontext;
+        this.canvas = this.context.canvas;
+        this.positions = this.getPositions(shirtnums, symbols,this.context);
+        this.prevpositions = null;
+        this.isMoving = false;
+        logmyobject("lineup positions at creation time",this.positions);
+
+        // Event listener for mouse move on the canvas
+        /*
+        this.canvas.addEventListener('mousemove', (event) => {
+            //console.log("I redrew because of mouse movement")
+            this.draw(); // Redraw the lineup on every mouse movement
+        });
+        */
+
+        this.mdref = this.onMouseDown.bind(this);
+        this.muref = this.onMouseUp.bind(this);
+        this.mmref = this.onMouseMove.bind(this);
+
+        this.addEventListeners();
+
+        //this.addPosListeners();
+
     }
 
-    getPositions(shirtnums, symbols) {
+
+    addEventListeners() {
+        // Add event listeners
+        this.canvas.addEventListener('mousedown', this.mdref);
+        this.canvas.addEventListener('mousemove', this.mmref);
+        this.canvas.addEventListener('mouseup', this.muref);
+    }
+
+    addPosListeners() {
+        // Event listener for mouse down on the canvas
+        this.canvas.addEventListener('mousedown', (event) => {
+            this.positions.forEach((pos,index) => {
+                logmyobject("calling mousedown on element index",index)
+                pos.onMouseDown(event); // Call onMouseDown for each position
+            });
+        });
+
+        // Event listener for mouse up on the canvas
+        this.canvas.addEventListener('mouseup', (event) => {
+            this.positions.forEach((pos,index) => {
+                logmyobject("calling mouseup on element index",index)
+                pos.onMouseUp(event); // Call onMouseUp for each position
+            });
+        });
+    }
+
+    onMouseDown(event) {
+        this.positions.forEach((pos,index) => {
+            logmyobject("calling mousedown on element index",index)
+            pos.onMouseDown(event); // Call onMouseDown for each position
+        });
+    }
+
+    onMouseUp(event) {
+        this.positions.forEach((pos,index) => {
+            logmyobject("calling mouseup on element index",index)
+            pos.onMouseUp(event); // Call onMouseUp for each position
+        });
+    }
+
+    onMouseMove(event) {
+        console.log("I redrew because of mouse movement");
+        this.draw();
+    }
+
+    // Function to remove all positions from the lineup
+    clearPositions() {
+        this.canvas.removeEventListener('mousedown', this.mdref);
+        this.canvas.removeEventListener('mousemove', this.mmref);
+        this.canvas.removeEventListener('mouseup', this.muref);
+        this.positions.forEach(pos => {
+            // Remove event listeners
+            this.canvas.removeEventListener('mousedown', pos.onMouseDown);
+            this.canvas.removeEventListener('mousemove', pos.onMouseMove);
+            this.canvas.removeEventListener('mouseup', pos.onMouseUp);
+            pos.removeEventListeners();
+        });
+
+        this.positions = []; // Clear the positions array
+    }
+
+    getPositions(shirtnums, symbols,lucontext) {
         var positions = []// [new Position(1),new Position(2),new Position(3),new Position(4),new Position(5),new Position(6),];
         var pos = 1;
         shirtnums.forEach(shirtnum => {
             console.log("pos:", pos)
             console.log("shirtnum:", shirtnum)
             var symbol = symbols[shirtnums.indexOf(shirtnum)]
-            positions.push(new Position(pos, shirtnum,symbol));
+            var updatedposition = new Position(pos, shirtnum,symbol,lucontext)
+            positions.push(updatedposition);
             console.log("assigned shirtnum ",shirtnum, " to position ",pos," successfully")
             pos ++;
         })
@@ -118,30 +202,76 @@ class Lineup {
     }
 
     rotateForward(n=1) {
+        //this.prevpositions = this.positions;
+        this.clearPositions();
         this.shirtnums = arrayRotateN(this.shirtnums, false,n);
         this.symbols = arrayRotateN(this.symbols, false,n);
-        this.positions = this.getPositions(this.shirtnums, this.symbols);
+        this.positions = this.getPositions(this.shirtnums, this.symbols, this.context);
+        //this.addPosListeners();
+        this.addEventListeners();
+        //this.updatePrevpos(n);
+        logmyobject("lineup positions after rotate forward",this.positions);
+        logmyobject("previous lineup positions after rotate forward",this.prevpositions);
     }
 
     rotateBackward(n=1) {
-        /* var counter = 0
-        while (counter < n) {
-            this.shirtnums = arrayRotate(this.shirtnums,true);
-            this.symbols = arrayRotate(this.symbols);
-            counter +=1;
-        } 
-        */
+        //this.prevpositions = this.positions;
+        this.clearPositions();
         this.shirtnums = arrayRotateN(this.shirtnums, true,n);
         this.symbols = arrayRotateN(this.symbols, true,n);
-        this.positions = this.getPositions(this.shirtnums, this.symbols);
+        this.positions = this.getPositions(this.shirtnums, this.symbols, this.context);
+        //this.addPosListeners();
+        this.addEventListeners();
+        //this.updatePrevpos(n, false);
+        logmyobject("lineup positions after rotate backward",this.positions);
+        logmyobject("previous lineup positions after rotate backward",this.prevpositions);
     }
 
-    draw(context) {
-        drawcourt();
+    isMoving() {
+        var output = true;
         this.positions.forEach( pos => {
-            pos.draw(context);
+            output = output & pos.isMoving;
+        })
+        return output
+    }
+
+    updatePrevpos(n=1, forward = true) {
+        this.positions.forEach((pos,index) => {
+            logmyobject("in updatePrevpos, index",index);
+            logmyobject("in updatePrevpos, n",n);
+            logmyobject("in updatePrevpos, forward",forward);
+            var previndex = (index +n) % 6
+            if (!forward) {
+                previndex = (index + 5 +n) % 6;
+            }
+            logmyobject("in updatePrevpos, previndex",previndex);
+            var prevpos = this.prevpositions[previndex ];
+            logmyobject("pos before updating prevpos",pos);
+            pos.prevxpos = prevpos.xpos;
+            pos.prevypos = prevpos.ypos;
+            logmyobject("pos after updating prevpos",pos);
+            logmyobject("prevpos",prevpos);
         })
     }
+
+    movePositions() {
+
+        while (this.isMoving()) {
+            this.positions.forEach( pos => {
+                pos.moveTowards()
+            })
+        }
+    }
+
+    draw() {
+        this.context.clearRect(0, 0, window_width , window_height)
+        drawcourt();
+        this.positions.forEach( pos => {
+            pos.draw();
+        })
+    }
+
+
 
 }
 
@@ -153,8 +283,13 @@ function getSymbolsFromSetterPosition(setterpos = 2) {
     return symbols2
 }
 
+function logmyobject(desc ="myobj",myobj) {
+    console.log(desc + ": ");
+    console.dir(myobj);
+}
+
 class Position {
-    constructor(value, shirtnum, symbol = "P") {
+    constructor(value, shirtnum, symbol = "P",poscontext) {
 
         if(!([1,2,3,4,5,6].includes(value))) {
             throw('value can only take any of the following values: [1,2,3,4,5,6], but value attempt was: '+ value.toString() );
@@ -164,18 +299,13 @@ class Position {
         this.shirtnum = shirtnum;
         this.symbol = symbol;
 
-
-
-        this.pos_width = 0.2*window_width;
-        this.pos_height = 0.1*window_height;
+        this.width = 0.2*window_width;
+        this.height = 0.1*window_height;
 
         this.speed = 50;
 
-        this.nbstepsx =  Math.abs(this.prevxpos-this.xpos) / this.speed 
-        this.nbstepsy = Math.abs(this.prevypos-this.ypos) / this.speed
-
-        this.prevxpos = 50;
-        this.prevypos = 50;
+        this.context = poscontext;
+        this.canvas = this.context.canvas;
 
         this.isfrontrow = ([2,3,4].includes(value));
         this.isbackrow = ([5,6,1].includes(value));
@@ -198,50 +328,145 @@ class Position {
         if (this.isrightside) {
             this.xpos = 0.75*window_width
         } 
+
+        this.prevxpos = this.xpos;
+        this.prevypos = this.ypos;
+
+        this.isMoving = false;
+        this.currentmovestep = 0;
+
+        this.isDragging = false;
+        this.dragOffsetX = 0;
+        this.dragOffsetY = 0;
+
+        this.mdref = this.onMouseDown.bind(this);
+        this.muref = this.onMouseUp.bind(this);
+        this.mmref = this.onMouseMove.bind(this);
+
+        this.addEventListeners();
+
+    }
+
+    addEventListeners() {
+        // Add event listeners
+        this.canvas.addEventListener('mousedown', this.mdref);
+        this.canvas.addEventListener('mousemove', this.mmref);
+        this.canvas.addEventListener('mouseup', this.muref);
+    }
+
+    removeEventListeners() {
+        this.canvas.removeEventListener('mousedown', this.mdref);
+        this.canvas.removeEventListener('mousemove', this.mmref);
+        this.canvas.removeEventListener('mouseup', this.muref);
     }
 
     prevposition() {
         var allvalues = [1,2,3,4,5,6]
         var currindex = allvalues.indexOf(this.value)
-        var previndex = (currindex +7) % 6
+        var previndex = (currindex +1) % 6
         return allvalues[previndex]
     }
+ 
+    moveFrom() {
 
-    
-    animate() {
-        var isanimationover = isanimationover = (this.xpos == this.prevxpos) * (this.xpos == this.prevxpos)
+        //var tolerance = 0.5
+        //var is_move_over = (isCloseEnough(this.xpos,fromxpos,tolerance) && isCloseEnough(this.ypos,fromypos,tolerance));
 
-        if (!isanimationover) {
-            this.xpos = this.prevxpos -Math.sign(this.prevxpos-this.xpos)*this.speed ;
-            this.ypos = this.prevypos -Math.sign(this.prevypos-this.ypos)*this.speed ;
-            requestAnimationFrame(animate);
-        } 
-    }
+        logmyobject("inside modeTowards, current object",this);
 
-    draw(context) {
-        context.beginPath();
-        context.strokeStyle = "green";
-        context.lineWidth = 3;
-        context.rect(
-            this.xpos - 0.5*this.pos_width,
-            this.ypos - 0.5*this.pos_height,
-            this.pos_width,
-            this.pos_height);
-        context.stroke();
-        context.closePath();
+        var nbsteps = 1/this.speed;
 
-        context.textAlign = "center";
-        context.textBaseline = "middle"
-        context.font = "20px Arial";
+        console.log("nbsteps:",nbsteps);
+        console.log("prevxpos:",this.prevxpos);
+        console.log("prevypos:",this.prevypos);
+        console.log("this.currentmovestep:",this.currentmovestep);
 
-        context.fillText(this.shirtnum + "/" + this.symbol, this.xpos, this.ypos);
-        context.lineWidth = 5;
-        if ((this.prevxpos != this.xpos) | (this.prevypos != this.ypos)) {
-            //draw transition
-            //requestAnimationFrame(animate);
+        this.isMoving =  (this.currentmovestep < nbsteps); // 
+
+        console.log("is_move_over:",is_move_over);
+
+        const dx = Math.abs(this.prevxpos-this.xpos) / nbsteps;
+        const dy = Math.abs(this.prevypos-this.ypos) / nbsteps;
+
+
+        if (!this.isMoving) {
+            this.xpos += dx ;
+            this.ypos += dy ;
+            this.draw()
+            this.currentmovestep  ++ ;
+        } else {
+          this.currentmovestep = 0;
+          this.prevxpos = this.xpos;
+          this.prevypos = this.ypos;
         }
 
+        return this.isMoving
     }
+
+    draw() {
+        var poscontext = this.context;
+        poscontext.beginPath();
+        poscontext.textAlign = "center";
+        poscontext.textBaseline = "middle"
+        poscontext.font = "20px Arial";
+        poscontext.strokeStyle = "green";
+        poscontext.lineWidth = 3;
+        poscontext.fillText(this.shirtnum + "/" + this.symbol, this.xpos, this.ypos);
+        poscontext.rect(
+            this.xpos - 0.5*this.width,
+            this.ypos - 0.5*this.height,
+            this.width,
+            this.height
+            );
+        poscontext.stroke();
+        poscontext.closePath();
+
+    }
+
+    onMouseDown(event) {
+        const rect = this.canvas.getBoundingClientRect();
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+
+        if (
+            mouseX >= this.xpos - 0.5 * this.width &&
+            mouseX <= this.xpos + 0.5 * this.width &&
+            mouseY >= this.ypos - 0.5 * this.height &&
+            mouseY <= this.ypos + 0.5 * this.height
+        ) {
+            console.log("you have grabbed this player")
+            this.isDragging = true;
+            this.dragOffsetX = mouseX - this.xpos;
+            this.dragOffsetY = mouseY - this.ypos;
+        } else {
+            console.log("you have missed this player")
+        }
+    }
+
+    onMouseMove(event) {
+        if (this.isDragging) {
+            console.log("you are moving this player")
+            const rect = this.canvas.getBoundingClientRect();
+            const mouseX = event.clientX - rect.left;
+            const mouseY = event.clientY - rect.top;
+
+            this.xpos = mouseX - this.dragOffsetX;
+            this.ypos = mouseY - this.dragOffsetY;
+
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.draw();
+        }
+    }
+
+    onMouseUp(event) {
+        if (this.isDragging) {
+            this.isDragging = false;
+        }
+    }
+}
+
+isCloseEnough = function(a,b,tolerance) {
+    return Math.abs(a-b) < Math.abs(tolerance)
 }
 
 class Player {
@@ -303,21 +528,38 @@ if (!test_mode) {
     }
 
     mysymbols = getSymbolsFromSetterPosition(3);
-    mylineup = new Lineup([5,9,45,23,12,7],mysymbols);
-    mylineup.draw(context);
+    mylineup = new Lineup([5,9,45,23,12,7],mysymbols,context);
+    mylineup.draw();
+
     
     document.getElementById('fwd').addEventListener('click',function(){
-        context.clearRect(0, 0, canvas.width, canvas.height)
+        //context.clearRect(0, 0, canvas.width, canvas.height)
         mylineup.rotateForward();
-        mylineup.draw(context);
+        mylineup.draw();
+        //animate();
     });
     
     document.getElementById('bck').addEventListener('click',function(){
-        context.clearRect(0, 0, canvas.width, canvas.height)
+        //context.clearRect(0, 0, canvas.width, canvas.height)
         mylineup.rotateBackward();
-        mylineup.draw(context);
+        mylineup.draw();
+        //animate();
+        
     });
+
+
+    /*
+    function animate(isOver = false) {
+        isOver = mylineup.draw(context);
+        if (!isOver) {
+            requestAnimationFrame(animate);
+        }
+    }
+    */
+    
 }
+
+
 
 
 //testing it all
